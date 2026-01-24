e955805d0d2e0f9235f81338e38ac1b576a91887
rubengutierrez2026 <g.rubendario42@gmail.com>
2026-01-24 12:57:59 -0300

    ingreso de dni y validacion de cliente

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// EL USUARIO INGRESA DNI QUE SE GUARDA EN VARIABLE 
+
+let dniIngresado = parseInt (prompt("Ingrese el DNI del cliente:"))
+console.log(dniIngresado)
+
+
+// VALIDACION DE CLIENTE 
+
+let cliente = 30570778
+
+if (cliente == dniIngresado){
+    console.log("Bienvenido al sistema: " + cliente)
+}else{
+    console.log("Usuario no encontrado")
+}
+
+
+
+      
+
+        
+       
+       
+        
+     
+
+
+
+
