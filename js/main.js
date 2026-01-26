0dfa2a96abb3284dae64aa9fffbe5f0b763ce0c5
rubengutierrez2026 <g.rubendario42@gmail.com>
2026-01-26 19:39:42 -0300

    entrega 1 falta funciones para bancos y agencias terminar while y terminar arrays

--- js/main.js
+++ js/main.js
@@ -1,29 +1,83 @@
+
+// BASE DE DATOS SIMULADA
+let Nombre = "Adrian Gimenez"     
+let dni = 30570778
+let Banco = "Macro"
+let Agencia ="Cobro ya"
+let EstadoDeuda = "Deudor"
+let MontoDeuda = 2000
+let sinMonto = 0
 // EL USUARIO INGRESA DNI QUE SE GUARDA EN VARIABLE 
 
 let dniIngresado = parseInt (prompt("Ingrese el DNI del cliente:"))
 console.log(dniIngresado)
 
+// VALIDACION DE CLIENTE CON CONDICIONALES
 
-// VALIDACION DE CLIENTE 
-
-let cliente = 30570778
-
-if (cliente == dniIngresado){
-    console.log("Bienvenido al sistema: " + cliente)
-}else{
-    console.log("Usuario no encontrado")
+   if (dni == dniIngresado){
+    let resultado = Nombre 
+    alert("Nombre de Cliente: " + Nombre)
+   }else {
+    alert("Cliente inexistente")
+   }
+ 
+ 
+// FUNCIONES
+function montoDeuda(){
+    let sinmonto =  sinMonto
+    let monto = MontoDeuda 
+    let resultadoDeuda =  (MontoDeuda + sinMonto)
+    // console.log("Monto Adeudado: " + resultadoDeuda )
+    alert("Monto Adeudado: " + resultadoDeuda)
 }
+montoDeuda()
 
+function estadodeuda(){
+   if ( MontoDeuda > 0){
+    // console.log("Cliente con Deuda activa.!!!")
+    alert("Cliente con Deuda activa.!!!")
+   }else{
+    // console.log("Cliente no registra Deuda.!!!")
+    alert("Cliente no registra Deuda.!!!")
+   }
+}
+estadodeuda()
 
+ 
 
-      
+    let menu = parseInt(prompt("Elija una opcion:  \n 1-Monto Deuda \n 2-Estado Deuda \n 3-Banco \n 4-Agencia \n 5-Salir"))
 
+while(menu !== 5){
+    switch(menu) {
+        case 1:
+          montoDeuda()
+          break
         
-       
-       
+        case 2:
+          estadodeuda()
+          break
+          
+        case 3:
+          bancos()  
+          break 
+        
+        case 4:
+          agencia()
+          break
+          
+        default:
+          alert("Opcion incorrecta")  
+         
+       }
+     menu = parseInt(prompt("Elija una opcion:  \n 1-Monto Deuda \n 2-Estado Deuda \n 3-Banco \n 4-Agencia \n 5-Salir"))
+
+}   
         
      
 
 
-
-
+const bancos = ["Macro","Galicia","Santander","Hbc"]
+console.log(bancos)
+ 
+const agencias =["Cobro ya","Credit","Asis","Cobromas"]
+console.log(agencias)
